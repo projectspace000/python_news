a420969644cf2455c20be599558791d14e370c1c
Mitchell Rapp <project.space.000@gmail.com>
2016-08-21 23:01:05 +0000

    removed period in print statement

--- news.py
+++ news.py
@@ -5,6 +5,6 @@ print "\n\n\nImports completed successfully.\n\n\nInitializing...\n"
 start = 0
 
 while True:
-    print("Initializing for " + str(start) + " seconds.")
+    print("Initializing for " + str(start) + " seconds")
     start = start + 1
     time.sleep(1)
