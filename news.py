6d63f71cc009f5ab7efa9aa6cf83345c2ec2af53
Mitchell Rapp <project.space.000@gmail.com>
2016-08-21 23:07:15 +0000

    beginning email generation

--- news.py
+++ news.py
@@ -1,10 +1,27 @@
+#!/usr/bin/env python
+#news.py
+
+import smtplib
 import time
-import sys
-print "\n\n\nImports completed successfully.\n\n\nInitializing...\n"
+from subprocess import Popen, PIPE
 
-start = 0
+# email variables and login action
+sender_email = "project.space.000@gmail.com"
+sender_password = "Namnori117!GMA"
+receipient_email = "mcdbrendan@gmail.com"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender_email, sender_password)
 
-while True:
-    print("Initializing for " + str(start) + " seconds")
-    start = start + 1
-    time.sleep(1)
+# build and send email
+send_time = time.strftime('%X %x %Z')
+header = "\n This email was automatically generated and sent at " + send_time +"\n"
+body = "\n Here's it today's rundown:  "
+msg = header + body
+server.sendmail(sender_email, receipient_email, msg)
+server.quit()
+
+print ("Email successfully sent")
+
+
+
