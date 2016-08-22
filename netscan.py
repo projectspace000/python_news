73f636ffed61cfc671139718fd460b1fd987cf71
Mitchell Rapp <project.space.000@gmail.com>
2016-08-22 17:15:05 +0000

    updated netscan to nmap -sV -O

--- netscan.py
+++ netscan.py
@@ -1,6 +1,7 @@
 import time
 import sys
+from subprocess import call
 
 # run nmap scan to examine current network connections, OS, and device name
-
+call(["sudo","nmap", "-sV", "-O","192.168.0.*"])
 
