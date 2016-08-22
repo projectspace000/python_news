741d5dfc72718bd1bd9165ffa4e13ca8f373b764
Mitchell Rapp <project.space.000@gmail.com>
2016-08-22 17:22:49 +0000

    updated nmap options to -sS -O

--- netscan.py
+++ netscan.py
@@ -3,5 +3,5 @@ import sys
 from subprocess import call
 
 # run nmap scan to examine current network connections, OS, and device name
-call(["sudo","nmap", "-sV", "-O","192.168.0.*"])
+call(["sudo","nmap", "-sS", "-O","192.168.0.*"])
 
